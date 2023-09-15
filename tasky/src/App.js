00f0895fd0bb80e3277e6f57bb8e40f5ad055e93
MingJu Sun <20104599@mail.wit.ie>
2023-09-15 16:27:31 +0100

    Completed Week 1 Lab

--- tasky/src/App.js
+++ tasky/src/App.js
@@ -1,23 +1,19 @@
 import logo from './logo.svg';
+import Task from './components/Task';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Tasky</h1>
+      <Task title="Dishes" deadline="Today">
+      <div className="style">Empty dishwasher</div>
+      </Task>
+      <Task title="Laundry" deadline="Tomorrow">
+        <div className="style">Fold laundry and put away</div>
+      </Task>
+      <Task title="Tidy" deadline="Today" />
     </div>
   );
 }
